1e840240410dd5dd50e7a8e4124e88217aa04bad
Bryan Kai <bryankai@GPs-MacBook-Pro.local>
2018-05-07 11:51:42 -0700

    Added functionality to the createPost, edit, and delete buttons.

--- src/helper.js
+++ src/helper.js
@@ -9,64 +9,103 @@ function request(path, method = 'get', body = null) {
   })
 }
 
-function populateView (response) {
+function populateSideBar() {
+  let id
+  request(`/posts`, 'get')
+    .then(allPosts => {
+      const { data } = allPosts.data
+      const sidebar = document.getElementById('sidebar')
+      const listGroup = document.createElement('ul')
+      listGroup.classList.add('list-group')
+      data.forEach(blogPost => {
+        const sidebar = document.getElementById('sidebar')
+        const post = document.createElement('div')
+        sidebar.appendChild(post)
+        post.classList.add('list-group-item', 'list-group-item-action')
+        post.id = blogPost.id
+        post.innerHTML = blogPost.title
+        post.addEventListener('click', event => {
+          request(`/posts/${event.target.id}`, 'get')
+            .then(response => {
+              clearSectionById('view')
+              populateView(response)
+            })
+        })
+      })
+      return allPosts
+    })
+}
+
+function populateView(response) {
   const post = response.data.data
+  console.log(post)
   // Populate the right column
   // section
   const view = document.getElementById('view')
   const section = document.createElement('section')
   view.appendChild(section)
-    // header
-    const header = document.createElement('header')
-    section.appendChild(header)
-      // h2
-      const h2 = document.createElement('h2')
-      header.appendChild(h2)
-      h2.innerHTML = post.title
-    // article
-    const article = document.createElement('article')
-    section.appendChild(article)
-      // p
-      const p = document.createElement('p')
-      article.appendChild(p)
-      p.innerHTML = post.content
-    // aside
-    const aside = document.createElement('aside')
-    section.appendChild(aside)
-      // ul
-      const ul = document.createElement('ul')
-      aside.appendChild(ul)
-      ul.classList.add('nav', 'justify-content-end')
-      // edit
-      const liEdit = document.createElement('li')
-      ul.appendChild(liEdit)
-      liEdit.classList.add('nav-item')
-      const editButton = document.createElement('div')
-      liEdit.appendChild(editButton)
-      editButton.classList.add('nav-link')
-      editButton.innerHTML = 'Edit'
-      editButton.addEventListener('click', event => {
-        clearView()
-        populateUpdatePostForm()
-      })
-      // delete
-      const liDelete = document.createElement('li')
-      ul.appendChild(liDelete)
-      liDelete.classList.add('nav-item')
-      const deleteButton = document.createElement('div')
-      liDelete.appendChild(deleteButton)
-      deleteButton.classList.add('nav-link', 'text-danger')
-      deleteButton.innerHTML = 'Delete'
+  // header
+  const header = document.createElement('header')
+  section.appendChild(header)
+  // h2
+  const h2 = document.createElement('h2')
+  header.appendChild(h2)
+  h2.innerHTML = post.title
+  // article
+  const article = document.createElement('article')
+  section.appendChild(article)
+  // p
+  const p = document.createElement('p')
+  article.appendChild(p)
+  p.innerHTML = post.content
+  // aside
+  const aside = document.createElement('aside')
+  section.appendChild(aside)
+  // ul
+  const ul = document.createElement('ul')
+  aside.appendChild(ul)
+  ul.classList.add('nav', 'justify-content-end')
+
+  // edit
+  const liEdit = document.createElement('li')
+  ul.appendChild(liEdit)
+  liEdit.classList.add('nav-item')
+  const editButton = document.createElement('div')
+  liEdit.appendChild(editButton)
+  editButton.classList.add('nav-link')
+  editButton.innerHTML = 'Edit'
+  editButton.addEventListener('click', event => {
+    clearSectionById('view')
+    populatePostForm(response, 'update')
+  })
+
+  // delete
+  const liDelete = document.createElement('li')
+  ul.appendChild(liDelete)
+  liDelete.classList.add('nav-item')
+  const deleteButton = document.createElement('div')
+  liDelete.appendChild(deleteButton)
+  deleteButton.classList.add('nav-link', 'text-danger')
+  deleteButton.innerHTML = 'Delete'
+  deleteButton.addEventListener('click', event => {
+    request(`/posts/${post.id}`, 'delete')
+    .then(putResponse => {
+      clearSectionById('sidebar')
+      populateSideBar()
+      clearSectionById('view')
+    })
+  })
 }
 
-function clearView() {
-  const view = document.getElementById('view')
-  while (view.firstChild) {
-    view.removeChild(view.firstChild);
+function clearSectionById(cssId) {
+  const section = document.getElementById(cssId)
+  while (section.firstChild) {
+    section.removeChild(section.firstChild);
   }
 }
 
-function populateUpdatePostForm () {
+function populatePostForm(response, type) {
+  const id = response.data.data.id
   const view = document.getElementById('view')
   const form = document.createElement('form')
   view.appendChild(form)
@@ -87,6 +126,58 @@ function populateUpdatePostForm () {
   titleInput.classList.add('form-control')
 
   //content group
+  const contentGroup = document.createElement('div')
+  form.appendChild(contentGroup)
+  contentGroup.classList.add('form-group')
+  const contentLabel = document.createElement('label')
+  contentGroup.appendChild(contentLabel)
+  contentLabel.for = 'content'
+  contentLabel.innerHTML = 'Content'
+  const contentInput = document.createElement('input')
+  contentGroup.appendChild(contentInput)
+  contentInput.type = 'text'
+  contentInput.id = 'content'
+  contentInput.innerHTML = 'Content'
+  contentInput.classList.add('form-control')
 
   //submit button
+  const submitButton = document.createElement('button')
+  form.appendChild(submitButton)
+  submitButton.type = "submit"
+  submitButton.classList.add('btn', 'btn-primary', 'btn-large')
+
+  // Update Post
+  if (type == 'update') {
+    submitButton.innerHTML = 'Update Post'
+    document.getElementById('post-form').addEventListener('submit', function(event) {
+      event.preventDefault()
+      const title = event.target.title.value
+      const content = event.target.content.value
+      request(`/posts/${id}`, 'put', { title, content })
+      .then(putResponse => {
+        clearSectionById('sidebar')
+        populateSideBar()
+        clearSectionById('view')
+        populateView(putResponse)
+      })
+    })
+  } else if (type == 'create') {
+    submitButton.innerHTML = 'Create Post'
+    document.getElementById('post-form').addEventListener('submit', function(event) {
+      event.preventDefault()
+      const title = event.target.title.value
+      const content = event.target.content.value
+      request(`/posts`, 'post', { title, content })
+      .then(postResponse => {
+        clearSectionById('sidebar')
+        populateSideBar()
+        clearSectionById('view')
+        populateView(postResponse)
+      })
+    })
+  }
+}
+
+function populateSubmitButton() {
+
 }
--- src/postsList.js
+++ src/postsList.js
@@ -1,42 +1,13 @@
 (function() {
   'use strict';
-  let id
-  console.log('sidebar!')
-    // load post information
-    request(`/posts`, 'get')
-    .then(response => {
-      const {data} = response.data
-      const sidebar = document.getElementById('sidebar')
-      const listGroup = document.createElement('ul')
-      listGroup.classList.add('list-group')
-      data.forEach(blogPost => {
-        const sidebar = document.getElementById('sidebar')
-        const post = document.createElement('div')
-        sidebar.appendChild(post)
-        post.classList.add('list-group-item', 'list-group-item-action')
-        post.id = blogPost.id
-        post.innerHTML = blogPost.title
-        post.addEventListener('click', event => {
-          request(`/posts/${event.target.id}`, 'get')
-          .then(response => {
-            clearView()
-            populateView(response)
-          })
-        })
-      })
+  populateSideBar()
+
+  // render createPostButton
+  request(`/posts`, 'get')
+  .then (response => {
+    const createPost = document.getElementById('create-post').addEventListener('click', function(event){
+      clearSectionById('view')
+      populatePostForm(response, 'create')
     })
-
-
-  // Create New Post Event Listener
-  // document.querySelector('.form-create-post').addEventListener('submit', function(event){
-  //   event.preventDefault()
-  //   const postName = event.target.name.value
-  //   const description = event.target.description.value
-  //   const users_id = id
-  //   console.log(users_id)
-  //   request(`/users/${id}/posts`, 'post', { postName , description, users_id })
-  //   window.location = '/dashboard.html'
-  // })
-
-
+  })
 })();
